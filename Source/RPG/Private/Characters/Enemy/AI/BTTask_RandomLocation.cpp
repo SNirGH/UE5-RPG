37ec94c5b69e0bfb8e3d8ea72d0a1f407be149ac
Sharon Nir <snir.bender@gmail.com>
2025-06-09 11:35:16 -0400

    BTTask Find Random Location

---
+++ Source/RPG/Private/Characters/Enemy/AI/BTTask_RandomLocation.cpp
@@ -0,0 +1,37 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Characters/Enemy/AI/BTTask_RandomLocation.h"
+
+#include "AIController.h"
+#include "NavigationSystem.h"
+#include "BehaviorTree/BlackboardComponent.h"
+#include "GameFramework/Character.h"
+
+UBTTask_RandomLocation::UBTTask_RandomLocation()
+{
+	NodeName = "Find Random Location";
+}
+
+EBTNodeResult::Type UBTTask_RandomLocation::ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory)
+{
+	const AAIController* AIController = OwnerComp.GetAIOwner();
+	if (!AIController) return EBTNodeResult::Failed;
+
+	const ACharacter* AICharacter = AIController->GetCharacter();
+	if (!AICharacter) return EBTNodeResult::Failed;
+
+	const FVector ActorLocation = AICharacter->GetActorLocation();
+	const UNavigationSystemV1* NavigationSystem = FNavigationSystem::GetCurrent<UNavigationSystemV1>(GetWorld());
+	if (!NavigationSystem) return EBTNodeResult::Failed;
+
+	if (FNavLocation ResultLocation; NavigationSystem->GetRandomReachablePointInRadius(ActorLocation, SearchRadius, ResultLocation))
+	{
+		UBlackboardComponent* BlackboardComponent = OwnerComp.GetBlackboardComponent();
+		BlackboardComponent->SetValueAsVector(Location.SelectedKeyName, FVector(ResultLocation.Location.X, ResultLocation.Location.Y, ActorLocation.Z));
+
+		return EBTNodeResult::Succeeded;
+	}
+	
+	return EBTNodeResult::Failed;
+}
---
+++ Source/RPG/Public/Characters/Enemy/AI/BTTask_RandomLocation.h
@@ -0,0 +1,28 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BehaviorTree/BTTaskNode.h"
+#include "BTTask_RandomLocation.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class RPG_API UBTTask_RandomLocation : public UBTTaskNode
+{
+	GENERATED_BODY()
+
+public:
+	UBTTask_RandomLocation();
+
+protected:
+	virtual EBTNodeResult::Type ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory) override;
+
+	UPROPERTY(EditAnywhere, Category = Variables, meta = (ToolTip = "Random Location Within The Nav Mesh"))
+	FBlackboardKeySelector Location;
+
+	UPROPERTY(EditAnywhere, Category = Variables, meta = (ToolTip = "Searchable Radius Within The Nav Mesh"))
+	float SearchRadius = 2500.0F;
+};
