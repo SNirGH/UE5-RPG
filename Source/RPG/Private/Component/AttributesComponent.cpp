5c5d457ec657b64e208c58db2e54ded229eaf46f
Sharon Nir <snir.bender@gmail.com>
2025-06-10 11:27:12 -0400

    Implemented Attribute Functions

--- Source/RPG/Private/Component/AttributesComponent.cpp
+++ Source/RPG/Private/Component/AttributesComponent.cpp
@@ -6,7 +6,47 @@
 
 UAttributesComponent::UAttributesComponent()
 {
-	PrimaryComponentTick.bCanEverTick = true;
+	PrimaryComponentTick.bCanEverTick = false;
+}
+
+void UAttributesComponent::RecieveDamage(float Damage)
+{
+	Health = FMath::Clamp(Health - Damage, 0.0F, MaxHealth);
+}
+
+void UAttributesComponent::UseStamina(float StaminaCost)
+{
+	Stamina = FMath::Clamp(Stamina - StaminaCost, 0.0F, MaxStamina);
+}
+
+void UAttributesComponent::AddGold(int32 AmountOfGold)
+{
+	Gold += AmountOfGold;
+}
+
+void UAttributesComponent::AddSouls(int32 AmountOfSouls)
+{
+	Souls += AmountOfSouls;
+}
+
+void UAttributesComponent::RegenStamina(float DeltaTime)
+{
+	Stamina = FMath::Clamp(Stamina + StaminaRegenRate * DeltaTime, 0.0F, MaxStamina);
+}
+
+float UAttributesComponent::GetHealthPercent()
+{
+	return Health / MaxHealth;
+}
+
+float UAttributesComponent::GetStaminaPercent()
+{
+	return Stamina / MaxStamina;
+}
+
+bool UAttributesComponent::IsAlive()
+{
+	return Health > 0.0F;
 }
 
 void UAttributesComponent::BeginPlay()
--- Source/RPG/Public/Component/AttributesComponent.h
+++ Source/RPG/Public/Component/AttributesComponent.h
@@ -15,22 +15,48 @@ class RPG_API UAttributesComponent : public UActorComponent
 public:
 	UAttributesComponent();
 
+	void RecieveDamage(float Damage);
+	void UseStamina(float StaminaCost);
+	void AddGold(int32 AmountOfGold);
+	void AddSouls(int32 AmountOfSouls);
+	void RegenStamina(float DeltaTime);
+
+	float GetHealthPercent();
+	float GetStaminaPercent();
+
+	bool IsAlive();
+
+	FORCEINLINE int32 GetGold() const { return Gold; }
+	FORCEINLINE int32 GetSouls() const { return Souls; }
+	FORCEINLINE int32 GetStamina() const { return Stamina; }
+
+	FORCEINLINE float GetDodgeCost() const { return DodgeCost; }
+
 protected:
 	virtual void BeginPlay() override;
 
 private:
-	UPROPERTY(EditAnywhere, Category = Attributes)
+	UPROPERTY(EditAnywhere, Category = "Actor Attributes")
 	float Health;
 
-	UPROPERTY(EditAnywhere, Category = Attributes)
+	UPROPERTY(EditAnywhere, Category = "Actor Attributes")
 	float MaxHealth;
 
-	UPROPERTY(EditAnywhere, Category = Attributes)
+	UPROPERTY(EditAnywhere, Category = "Actor Attributes")
 	float Stamina;
 
-	UPROPERTY(EditAnywhere, Category = Attributes)
+	UPROPERTY(EditAnywhere, Category = "Actor Attributes")
 	float MaxStamina;
 
-	UPROPERTY(EditAnywhere, Category = Attributes)
+	UPROPERTY(EditAnywhere, Category = "Actor Attributes")
+	float DodgeCost = 14.F;
+
+	UPROPERTY(VisibleAnywhere, Category = "Actor Attributes")
+	float StaminaRegenRate;
+
+	UPROPERTY(VisibleAnywhere, Category = "Actor Attributes")
 	int32 Gold;
+
+	UPROPERTY(VisibleAnywhere, Category = "Actor Attributes")
+	int32 Souls;
 };
